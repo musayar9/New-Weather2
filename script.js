4be5fd6b9c596e948e849f1c404f819918fddf8e
Musa Sayar <musasayar67@gmail.com>
2022-12-13 21:16:17 +0300

    Weather

---
+++ script.js
@@ -0,0 +1,133 @@
+
+const url = 'https://api.openweathermap.org/data/2.5/'
+const apıKey = '1507d0c921d7c41f67e2a586b769ef52';
+
+const container = document.querySelector(".container")
+const textSearch = document.querySelector("#textSearch");
+const localBtn = document.querySelector("#btnLocal")
+const temp = document.querySelector(".temp")
+const wheather = document.querySelector(".wheather")
+const wheatImg = document.querySelector("img");
+const city2 = document.querySelector(".city")
+const feel = document.querySelector(".feel")
+const hummidity = document.querySelector(".humdty")
+const maxTemp = document.querySelector(".maxTemp")
+const minTemp = document.querySelector(".minTemp")
+const turnSearch = document.querySelector(".fa-arrow-circle-left")
+const wind = document.querySelector(".wind")
+const icon = document.querySelector(".icon")
+const notFound = document.querySelector(".not-found")
+const errorText = document.querySelector(".error-text")
+const errorWeather = document.querySelector(".error-weather")
+
+
+textSearch.addEventListener("keyup", e => {
+    if (e.key == "Enter" && textSearch.value != "") {
+        textValue = textSearch.value;
+        showWheather(textValue);
+        console.log(textValue)
+        showDayWeather(textValue);
+    }
+})
+async function showWheather(city) {
+    try {
+        const response = `${url}weather?q=${city}&appid=${apıKey}&units=metric&lang=tr`
+        const res = await fetch(response);
+        console.log(res)
+        const data = await res.json()
+        console.log(data)
+        if (data.cod === "404") {
+            document.querySelector(".not-found").style.opacity = 1;
+            document.querySelector(".not-found").style.height = "200 px";
+            errorText.innerText = `Ooops! ${data.message}`
+            document.querySelector(".card-body").classList.add("d-none")
+            textSearch.value = ""
+        } else {
+            document.querySelector(".not-found").style.opacity = 0;
+            document.querySelector(".not-found").style.height = 0;
+        }
+        city2.innerText = `${data.name}, ${data.sys.country}`
+        temp.innerText = `${data.main.temp}°C`
+        wheather.innerText = `${data.weather[0].description}`
+        hummidity.innerText = `${data.main.humidity} %`;
+        feel.innerText = `${data.main.feels_like}° c`
+        maxTemp.innerText = `${data.main.temp_max}° c`
+        minTemp.innerText = `${data.main.temp_min} ° c`
+        wind.innerText = `${parseInt(data.wind.speed)} Km/h`
+        const jpg = data.weather[0].id
+        console.log(jpg)
+        if (jpg == 800) {
+            wheatImg.src = "ımg/clear.svg";
+            icon.classList = "fa-solid fa-temperature-full text-danger me-2"
+        } else if (jpg >= 200 && jpg <= 232) {
+            wheatImg.src = "ımg/storm.svg";
+            icon.classList = "fa-solid fa-temperature-2 text-danger me-2"
+        }
+        else if (jpg >= 600 && jpg <= 622) {
+            wheatImg.src = "ımg/snow.svg"
+            icon.classList = "fa-solid fa-temperature-empty text-danger me-2"
+        }
+        else if (jpg >= 701 && jpg <= 781) {
+            wheatImg.src = "ımg/haze.svg";
+            icon.classList = "fa-solid fa-temperature-2 text-danger me-2"
+        } else if (jpg >= 801 && jpg <= 804) {
+            wheatImg.src = "ımg/cloud.svg"
+            icon.classList = "fa-solid fa-temperature-low text-danger me-2"
+        } else if ((jpg >= 500 && jpg <= 531) || (jpg >= 300 && jpg <= 321)) {
+            wheatImg.src = "ımg/rain.svg";
+            icon.classList = "fa-solid fa-temperature-half text-danger me-2"
+        }
+        textSearch.value = "";
+        container.classList.add("active")
+        document.querySelector(".not-cıty").style.opacity = 1;
+        document.querySelector(".not-cıty").style.height = "250px";
+        const dayContainer = document.querySelector(".daycontainer")
+        dayContainer.style.display = "flex"
+    } catch (err) {
+
+    }
+}
+localBtn.addEventListener("click", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onselect)
+    }
+    async function onselect(location) {
+        let lat = location.coords.latitude;
+        let lang = location.coords.longitude;
+        console.log(lang, lat)
+        const api_key = "56c0c78c732641268b0867e65fa8256c"
+        const locUrl = ` https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lang}&key=${api_key}`
+        const rsp = await fetch(locUrl)
+        console.log(rsp)
+        const data1 = await rsp.json()
+        console.log(data1)
+        const localHome = data1.results[0].components.town
+        console.log(localHome)
+        textSearch.value = localHome;
+        showWheather(localHome)
+        showDayWeather(localHome)
+
+        if (data1.cod === "404") {
+            document.querySelector(".not-found").style.opacity = 1;
+            document.querySelector(".not-found").style.height = "200 px";
+            errorText.innerText = `Ooops! ${data1.message}`;
+            document.querySelector(".card-body").classList.add("d-none")
+        } else {
+            document.querySelector(".not-found").style.opacity = 0;
+            document.querySelector(".not-found").style.height = "";
+            // document.querySelector(".card-body").classList.add("d-block")
+        }
+    }
+
+})
+
+turnSearch.addEventListener('click', () => {
+    container.classList.remove("active")
+    document.querySelector(".not-found").style.opacity = 0;
+    document.querySelector(".not-found").style.height = "";
+    document.querySelector(".card-body").classList.remove("d-none")
+    const dayContainer = document.querySelector(".daycontainer")
+    dayContainer.style.display = "none"
+})
+
+
---
+++ weather.js
@@ -0,0 +1,63 @@
+
+
+showDayWeather = (city) => {
+
+  const request = new XMLHttpRequest()
+  request.open("GET", `https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=${city}`);
+
+ request.setRequestHeader("content-type", "application/json");
+  request.setRequestHeader("authorization", "apikey 6xwuL85lXTUNtcpbBX9lLv:5JTHshh5oEzK4q8ZNERDbz");
+  request.send()
+  request.addEventListener('load', function () {
+    console.log(this)
+    console.log(this.responseText)
+    const data = JSON.parse(this.responseText);
+    console.log(data.success)
+
+    if (data.success == false) {
+      document.querySelector(".not-found").style.opacity = 1;
+      document.querySelector(".not-found").style.height = "200 px";
+      const errorDay = document.querySelector(".errorDay")
+      errorDay.innerText = `${data.message}`
+      document.querySelector(".card-body").classList.add("d-none")
+      textSearch.value = ""
+    } else {
+      document.querySelector(".not-found").style.opacity = 0;
+      document.querySelector(".not-found").style.height = 0;
+    }
+    const weatherResult = data.result;
+    console.log(weatherResult)
+    const dayTitle = document.querySelector('#dayTitle');
+    dayTitle.innerText = `${weatherResult[0].day} / ${weatherResult[0].date}`
+    let dayWeatherStatus = "";
+    for (let day of weatherResult) {
+      console.log(day)
+      dayWeatherStatus += `
+        <div class="card daycard" >
+        <div class="cardBody text-center">
+        <h5 class="card-title" title="Tarih">${day.date}</h5>
+          <h5 class="card-title" title="Gün">${day.day}</h5>
+          <img src="${day.icon}" id="dayImg" class="mb-2 mt-2" >
+          <p class="text-capitalize card-subtitle dayTitle mb-2 mt-2" title="Hava Durumu">${day.description}</p>
+          <p class=" dayTitle mb-2 text-danger" title="Sıcaklık"><i class="fa-solid fa-temperature-2 "></i> ${day.degree}°C</p>  
+          <p title= "Gece Sıcaklığı" class="text-black dayTitle">   <i class="fa-solid fa-moon "></i> ${day.night}°C</p>         
+          <p class="text-primary dayTitle" title="Nem Oranı">  <i class="fa-solid fa-smog text-primary"></i> %${day.humidity}</p>
+        </div>
+      </div>
+    `
+      const img = day.status;
+      document.querySelector(".daycontainer").innerHTML = dayWeatherStatus;
+    }
+    textSearch.value = "";
+    const firstDay = document.querySelector(".daycontainer")
+    console.log(firstDay)
+    for (i = 0; i < firstDay.children.length; i++) {
+      if (i == 0) {
+        firstDay.children[i].classList.add("d-none")
+      }
+    }
+  })
+}
+
+
+
